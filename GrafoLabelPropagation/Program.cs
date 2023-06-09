022813474e466a8a8e6155b565075374a7b1aa26
VictorVieiraV <vctin123@gmail.com>
2023-06-08 23:04:15 -0300

    Ajustes

--- GrafoLabelPropagation/Program.cs
+++ GrafoLabelPropagation/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Program
+﻿using GrafoLabelPropagation;
+
+public class Program
 {
     public static void Main(string[] args)
     {
@@ -11,11 +13,32 @@
         grafo.AddAresta(0, 1);
         grafo.AddAresta(1, 2);
         grafo.AddAresta(2, 0);
+        grafo.RemoveAresta(2, 0);
+        grafo.PonderarVertice(0, "PonderarVertice");
+        grafo.PonderarAresta(0, 1, "PonderarAresta");
 
-        bool isAdjacent = grafo.VerticeEAdjacente(0, 1);
-        Console.WriteLine("0 e 1 são adjacentes? " + isAdjacent);
+        bool eAdjacente = grafo.VerticeEAdjacente(0, 1);
+        Console.WriteLine("Vertices 0 e 1 são adjacentes? " + eAdjacente);
 
-        bool isComplete = grafo.GrafoEstaCompleto();
-        Console.WriteLine("O grafo é completo? " + isComplete);
+        bool eArestaAdjacente = grafo.ArestaEAdjacente(0, 1, 1, 2);
+        Console.WriteLine("Aresta 0,1 é adjacente a 1,2? " + eArestaAdjacente);
+
+        bool eIncidente = grafo.ArestaEIncidenteAVertice(0, 1, 2);
+        Console.WriteLine("Aresta 1,2 é incidente ao vertice 0? " + eIncidente);
+
+        bool eVazio = grafo.GrafoEstaVazio();
+        Console.WriteLine("O grafo é vazio? " + eVazio);
+
+        bool eComplete = grafo.GrafoEstaCompleto();
+        Console.WriteLine("O grafo é completo? " + eComplete);
+
+        bool existeAresta = grafo.ExisteAresta();
+        Console.WriteLine("No grafo existem arestas? " + existeAresta);
+
+        int numTotalVertices = grafo.GetNumVertices();
+        Console.WriteLine("Número total vertices: " + numTotalVertices);
+
+        int numTotalArestas = grafo.GetNumArestas();
+        Console.WriteLine("Número total arestas: " + numTotalArestas);
     }
 }
